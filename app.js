e08c6cc02fe9390f848da0a74045c497c2ded4ae
sourabh <saurabhchaurasiya398@gmail.com>
2024-09-06 13:49:06 +0530

    Added separation of concerns

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const app = express();
-
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const ownersRouter = require("./routes/ownersRouter")
+const usersRouter = require("./routes/usersRouters")
+const productRouter = require("./routes/productsRouter")
+
+const db = require("./config/mongoose-connection");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(express.static(path.json(_dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
 
-app.get("/",(req,res)=>{
-    res.send("hey");
-})
+app.use("/owners", ownersRouter);
+app.use("/users",usersRouter);
+app.use("/products",productRouter);
 
 app.listen(3000);
